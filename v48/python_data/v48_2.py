867c272c7dec520eee29aae9a3c65b742e84b7e6
henningpt <henning.ptaszyk@udo.edu>
2017-04-28 23:12:16 +0200

    python code

--- v48/python_data/v48_2.py
+++ v48/python_data/v48_2.py
@@ -2,24 +2,29 @@ import numpy as np
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 import scipy.constants as sc
-#daten laden
-#10^-11 , 0.1
+# daten laden
+# 10^-11 , 3
+I1, T1 = np.genfromtxt("2v48m1.txt", unpack=True)
 
-I1, T1 = np.genfromtxt("2v48m1.txt", unpack = True)
-#10^-11 , 0.3
+# 10^-11 , 0.3
 I2, T2 = np.genfromtxt("2v48m12.txt", unpack=True)
-#10^-11 , 1.0
+
+# 10^-11 , 1.0
 I3, T3 = np.genfromtxt("2v48m13.txt", unpack=True)
-#10^-10, 0.3
+
+# 10^-10, 0.3
 I4, T4 = np.genfromtxt("2v48m14.txt", unpack=True)
 
+# 10^-11 1.0
 I5, T5 = np.genfromtxt("2v48m15.txt", unpack=True)
+
 # groessen definieren
-I1 *= 0.1 * 10**(-11)
+I1 *= 3 * 10**(-11)
 I2 *= 0.3 * 10**(-11)
 I3 *= 1.0 * 10**(-11)
 I4 *= 0.3 * 10**(-10)
-I5 *= 0.3 * 10**(-10)
+I5 *= 1.0 * 10**(-11)
+
 # haenge daten aneinander
 I12 = np.append(I1, I2)
 I34 = np.append(I3, I4)
@@ -33,23 +38,29 @@ T = np.append(T14, T5)
 
 # funktionen
 
-def logj(Tinv,a,W):
-    return(a - Tinv * W/(sc.k))
 
+def logj(Tinv, a, W):
+    return(a - Tinv * W/(sc.k))
 
 
 # rechnung
 
 Ic = I[:16]
 Tc = T[:16]
-#params, cov = curve_fit(j, Tc, Ic)
+
+Ilog = np.log(I)
+Tinv = 1/T
+
+# params, cov = curve_fit(j, Tc, Ic)
 
 
 # plotten
-#x_plot = np.linspace(-61.8, 63.8, 1000)
+# x_plot = np.linspace(-61.8, 63.8, 1000)
 plt.plot(T, I, 'rx')
-#plt.plot(x_plot, j(x_plot,*params), 'b')
+
+# plt.plot(x_plot, j(x_plot,*params), 'b')
 plt.show()
 
 
-#ausgabe
+# ausgabe
+print(Tinv)
