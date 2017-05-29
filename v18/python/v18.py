e2319c39318b3cfafb96e152413b25bb4dd05f2e
henningpt <henning.ptaszyk@udo.edu>
2017-05-29 13:33:43 +0200

    v18 hinzugefuegt

---
+++ v18/python/v18.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.constants as sc
+import unumpy as unp
+import unp.ufloat
+
+# daten einlesen
+tEu = ufloat(4943, 5)
+tdiff = 6084
+
+
+# rechnungen
+
+
+# plotten
+
+
+# ergebnisse
