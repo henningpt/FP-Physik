a9042b9c6ae2800581ba4017d0a2a4320e69b6bb
henningpt <henning.ptaszyk@udo.edu>
2017-06-01 11:49:25 +0200

    txt files

--- v18/python/v18.py
+++ v18/python/v18.py
@@ -1,18 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.constants as sc
-import unumpy as unp
-import unp.ufloat
+from uncertainties import unumpy as unp
+from uncertainties import ufloat
 
 # daten einlesen
 tEu = ufloat(4943, 5)
 tdiff = 6084
 
+daten1 = np.genfromtxt("v18_1.txt", unpack=True)
+daten2 = np.genfromtxt("v18_2.txt", unpack=True)
+daten3 = np.genfromtxt("v18_3.txt", unpack=True)
+daten4 = np.genfromtxt("v18_4.txt", unpack=True)
 
 # rechnungen
 
 
 # plotten
-
-
+# a)
+plt.bar(list(range(len(daten1))), daten1, color='b')
+plt.show()
 # ergebnisse
