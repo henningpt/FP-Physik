79579c9c05ccade055c6b59ccaf1403cc7a7125b
howiboarder <mark.schoene@tu-dortmund.de>
2017-06-06 13:03:31 +0200

    Rechnung zu (a)

--- v18/python/peaks.py
+++ v18/python/peaks.py
@@ -1,19 +1,37 @@
 
 import numpy as np
+import math
 from scipy.optimize import curve_fit
-
+import uncertainties.unumpy as unp
 WERTE=np.genfromtxt('v18_1.txt' , unpack=True)
 
-Y=np.array([825,996,1154,1382,1491,2611,2907,3231])
+Y=np.array([414.085320642,
+825.610503916,
+996.656026607,
+1158.50557442,
+1382.07913025,
+1491.85090388,
+2273.6046823,
+2310.7129048,
+2612.40170034,
+2908.13276148,
+3231.86620663,
+3368.34965244,
+3639.14375894,
+3726.05188115,
+4353.12130571,
+4716.3497603,
+4888.96958224])
 
-X=np.array([244,295,344,411,443,678,778,867])
+X=np.array([121.78,244.70,295.94, 344.30, 411.12, 443.96, 678.00, 688.67, 778.90, 867.37, 964.08,1005.30,1085.90,1112.10,1299.10,1408.00,1457.60])
 
 A=np.array([121,244,295,344,411,443,678,688,778,867,964,1005,1085,1112,1299,1408,1457])
-print(A)
+#print(A)
 def f(x,a,b):
     return a*x+b
 
 params,covariance=curve_fit(f,X,Y)
+print(params)
 print('Kalibrierung', f(A,*params))
 
 #Funktionen
@@ -28,17 +46,58 @@ def gauss(a,s):
 
  params2,covariance2 = curve_fit(g,X2,Y2, p0=[1,a,0,1])
  
+
  #Plot Vorbereiten
  import matplotlib.pyplot as plt
- plt.rcParams['figure.figsize'] = (10, 8)
- plt.rcParams['font.size'] = 16
+ #plt.rcParams['figure.figsize'] = (10, 8)
+ #plt.rcParams['font.size'] = 16
  
- plt.plot(X2,Y2,'rx')
- plt.plot(X2,g(X2,*params2))
+ #plt.plot(X2,Y2,'rx')
+ #plt.plot(X2,g(X2,*params2))
  #plt.show() 
- 
- return params2
+ return params2, covariance2
 
-print('Parameter',gauss(1150,20))
+#Abgelesene Y Werte mit Gauss fitten und in Y eintragen
+C=[]
+for i in range(len(Y)):
+    c,d=gauss(Y[i],20)
+    C=C+[c]
+    Y[i]=C[i][1] 
+#Fuer bessere Werte nochmal
+C=[]
+D=[]
+for i in range(len(Y)):
+    c,d=gauss(Y[i],20)
+    C=C+[c]
+    D=D+[d]
+    Y[i]=C[i][1] 
+#Zeige werte an
+#for i in range(len(C)):
+ #   print(Y[i])
+    
+#print('Parameter',gauss(413,20))
+
+#Setzte Y Fehler
+Y_err=np.zeros(len(Y))
+for i in range(len(Y)):
+    Y_err[i] = np.sqrt(np.diag(D[i]))[1]
+y=unp.uarray(Y,Y_err)
+#Berechne Inhalte der Peaks mit der Integralformel fuer gauss
+
+I=np.zeros(len(Y))
+a=unp.uarray(np.zeros(len(Y)),np.zeros(len(Y)))
+for i in range(len(Y)):
+    a[i]=[C[i][0],np.sqrt(np.diag(D[i]))[0]]
+for i in range(len(Y)):
+    I[i]=C[i][3]*math.sqrt(np.pi)/math.sqrt(C[i][0])
+print(I)
+
+#Berechne die Effizienz
+omega=0.2
+Aktivitaet=1800
+W=np.array([28.6,7.6,0.4,26.5,2.2,3.1,2.0,0.9,12.9,4.2,14.6,0.6,10.2,13.6,1.6,21.0,0.5])
+Q=4*np.pi/omega/Aktivitaet*I/W
+
+print('Effizienz ',Q)
 
 np.savetxt('Guess.txt',f(A,*params),delimiter=' ; ',newline=' ; ',fmt='%3d')
